d965160da428b2e08a5909c87458c300c12340f2
Mauli <mauligulati@gmail.com>
2025-07-25 20:12:31 +0800

    [Feature] Organise folder structure, Add LocationDataService, Add Location model

---
+++ TourMap/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  TourMap
+//
+//  Created by Mauli Gulati on 25/7/25.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
