28067edb6ed86eae416b55b198d289c16db5987a
Mauli <mauligulati@gmail.com>
2025-07-25 15:47:52 +0800

    Initial Commit

---
+++ TourMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TourMap
+//
+//  Created by Mauli Gulati on 25/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TourMap/TourMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TourMapApp.swift
+//  TourMap
+//
+//  Created by Mauli Gulati on 25/7/25.
+//
+
+import SwiftUI
+
+@main
+struct TourMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
