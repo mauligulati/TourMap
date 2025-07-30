d3f70c512a503edccf6148b3d0ac1f42c723b025
Mauli <mauligulati@gmail.com>
2025-07-30 13:51:24 +0800

    [Feature] Add LocationPreviewView to LocationsView and handle transition

--- TourMap/Views/LocationsView.swift
+++ TourMap/Views/LocationsView.swift
@@ -27,6 +27,25 @@ struct LocationsView: View {
                     .padding()
                 
                 Spacer()
+                
+                ZStack {
+                    ForEach(locationsViewModel.locations) { location in
+                        if locationsViewModel.currentMapLocation == location {
+                            LocationPreviewView(location: location)
+                                .shadow(color: Color.black.opacity(0.3), radius: 20)
+                                .padding()
+                                .transition(
+                                    .asymmetric(
+                                        insertion: .move(edge: .trailing),
+                                        removal: .move(edge: .leading)
+                                    )
+                                )
+                        }
+                        
+                    }
+                }
+                
+                
             }
         }        
     }
